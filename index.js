58092a26cd0133e5f448c412a02e40b967587c18
Jaya Vishwakarma <jayavish423@gmail.com>
2023-01-29 19:26:50 +0530

    changes
    update
    
    Co-Authored-By: Swati Saulanki <100274438+swatisaulanki@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("HellO Worlds");
 console.log("HellO Worlds");
-console.log("HellO Worlds");
-console.log("HellO Worlds");
+// console.log("HellO Worlds");
+// console.log("HellO Worlds");
 function add(a,b){
     return a+b;
 
